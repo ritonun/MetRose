5825ba9ea89b4d42fe2503bc606b03a377ef46a8
ritonun <73615644+ritonun@users.noreply.github.com>
2025-09-17 00:47:56 +0200

    change line color to correspond to real life and station color to not look terrible

--- src/map_visu.py
+++ src/map_visu.py
@@ -8,16 +8,19 @@ def create_map(lines_coords, all_stations, train_positions=None, zoom_start=13):
     m = folium.Map(location=lines_coords[0][0], zoom_start=zoom_start)
 
     # Add metro line
-    color = ["red", "blue"]
+    color = ["red", "yellow"]
     i = 0
     for line_coords in lines_coords:
         folium.PolyLine(line_coords, color=color[i], weight=5, opacity=0.7).add_to(m)
         i += 1
 
     # Add stations
+    color = ["white", "purple"]
+    i = 0
     for stations in all_stations:
         for s in stations:
-            folium.CircleMarker(location=s, radius=5, color='red', fill=True).add_to(m)
+            folium.CircleMarker(location=s, radius=5, color=color[i], fill=True).add_to(m)
+        i += 1
 
     # Add simulated trains
     if train_positions:
